c3440809bed352ed918aa4fe1457073c30bdd522
Alan Henrique <mac@Alans-MacBook-Air.local>
2022-06-14 15:24:28 -0300

    ajusts quantity and minimum quantity erros

--- src/modules/products/services/UpdateProductService.ts
+++ src/modules/products/services/UpdateProductService.ts
@@ -45,18 +45,20 @@ class UpdateProductService {
     product.description = description
       ? String(description).toUpperCase().trim()
       : product.description;
+
     product.internal_code = internal_code
       ? String(internal_code).toUpperCase().trim()
       : product.internal_code;
+
     product.number_code = number_code
       ? String(number_code).toUpperCase().trim()
       : product.number_code;
+
     product.specific_code = specific_code
       ? String(specific_code).toUpperCase().trim()
       : product.specific_code;
-    product.observation = observation
-      ? String(observation).toUpperCase().trim()
-      : product.observation;
+
+    product.observation = String(observation).toUpperCase().trim();
 
     product.sector = sector
       ? String(sector).toUpperCase().trim()
@@ -66,9 +68,7 @@ class UpdateProductService {
       ? String(company).toUpperCase().trim()
       : product.company;
 
-    product.quantity = Number(quantity)
-      ? Number(quantity)
-      : Number(product.quantity);
+    product.quantity = Number(quantity);
 
     product.minimum_quantity = Number(minimum_quantity);
 
